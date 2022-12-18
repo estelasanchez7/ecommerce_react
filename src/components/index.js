cd16cd7875b17fc76af55e709fd4f1fe5fd4f27f
estelasanchez7 <stelasv7@gmail.com>
2022-12-18 10:46:49 +0100

    Highlights and Featured

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
+export {default as FlexContent} from './FlexContent.jsx'
 export {default as Hero } from './Hero.jsx'
-export {default as Sales} from './Sales'
+export {default as Sales} from './Sales.jsx'
