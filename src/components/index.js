59d279f658e7228e790d336fa3fa0e0435bf06c8
estelasanchez7 <stelasv7@gmail.com>
2022-12-20 11:27:47 +0100

    Navbar

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export {default as FlexContent} from './FlexContent.jsx'
 export {default as Hero } from './Hero.jsx'
 export {default as Sales} from './Sales.jsx'
 export {default as Footer} from './Footer.jsx'
+export {default as Navbar} from './Navbar.jsx'
 
